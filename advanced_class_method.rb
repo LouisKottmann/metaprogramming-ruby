6327feee274f2ec54f998e7a4b823139d4b6b36f
Louis Kottmann <louis.kottmann@gmail.com>
2013-05-27 22:42:01 +0200

    dried up advanced_class_method

--- advanced_class_method.rb
+++ advanced_class_method.rb
@@ -20,25 +20,31 @@ module RubyLearning
 		def alias_helper(method, characteristic)
 			method_name = method.to_s
 			characteristic_name = characteristic.to_s
+
 			#this is to put forward any punctuation towards the end of the method's new name
 			if %w(= ? !).any?{ |c| method_name.end_with?(c) }
 				punc = method_name[-1]
 				method_name = method_name[0..-2]
 				characteristic_name += punc
 			end
-			
-			alias_method "#{method_name}_without_#{characteristic_name}", method
+
+      method_with, method_without, method_intermediate =
+          "#{method_name}_with_#{characteristic_name}",
+          "#{method_name}_without_#{characteristic_name}",
+          "intermediate_#{method_name}_with_#{characteristic_name}"
+
+			alias_method method_without, method
 			
 			#this will enforce validation through a block passed as parameter
-			define_method("intermediate_#{method_name}_with_#{characteristic_name}") do
+			define_method(method_intermediate) do
 				if yield(instance_variable_get("@#{characteristic}"))
-					send("#{method_name}_with_#{characteristic_name}")
+					send(method_with)
 				else					
-					send("#{method_name}_without_#{characteristic_name}")
+					send(method_without)
 				end
 			end
 			
-			alias_method method, "intermediate_#{method_name}_with_#{characteristic_name}"
+			alias_method method, method_intermediate
 		end	
 	end
 end
